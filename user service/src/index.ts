0546c752a12a8270241d6feecd5fb57ee0470125
Vaibhav Pandey <vaibhavpandey3186@gmail.com>
2026-01-05 23:34:26 +0530

    fix: update server listening logic to use dynamic port from environment variable

--- user service/src/index.ts
+++ user service/src/index.ts
@@ -29,3 +29,10 @@ app.use("/api/v1", userRoutes);
 app.get("/", (req, res) => {
   res.send("Server is working");
 });
+
+const port = process.env.PORT || 5000;
+
+app.listen(5000, () => {
+  console.log(`Server is running on port ${port}`);
+  connectDb();
+});
