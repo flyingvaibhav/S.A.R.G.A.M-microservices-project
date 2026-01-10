e16deb1b7ccf4628bc369a72e78d47257dd1352a
Vaibhav Pandey <vaibhavpandey3186@gmail.com>
2026-01-10 22:10:03 +0530

    define IUser schema for user model with validation and timestamps

--- user service/src/model.ts
+++ user service/src/model.ts
@@ -8,3 +8,36 @@ export interface IUser extends Document {
   playlist: string[];
 }
 
+const schema: Schema<IUser> = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+
+    playlist: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model<IUser>("User", schema);
