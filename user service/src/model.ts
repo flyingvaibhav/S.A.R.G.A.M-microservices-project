1d0726fbaa50de3c72cde684c53a6462da65df67
Vaibhav Pandey <vaibhavpandey3186@gmail.com>
2026-01-10 22:09:46 +0530

    define IUser interface for user model

---
+++ user service/src/model.ts
@@ -0,0 +1,10 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+  playlist: string[];
+}
+
