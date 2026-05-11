2227ec33f7ede308f94803fda2ab714df4512ccd
Vaibhav Pandey <vaibhavpandey3186@gmail.com>
2026-05-11 16:04:23 +0530

    refactor: restructure index.js for improved readability and connection handling

--- user service/dist/index.js
+++ user service/dist/index.js
@@ -1,7 +1,30 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import userRoutes from "./route.js";
+import cors from "cors";
+dotenv.config();
+const connectDb = async () => {
+    try {
+        mongoose.connect(process.env.MONGO_URI, {
+            dbName: "Spotify",
+        });
+        console.log("Mongo Db Connected");
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
 const app = express();
-const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.use("/api/v1", userRoutes);
+app.get("/", (req, res) => {
+    res.send("Server is working");
+});
+const port = process.env.PORT || 5000;
 app.listen(5000, () => {
-    console.log(`User service is running on port ${PORT}`);
+    console.log(`Server is running on port ${port}`);
+    connectDb();
 });
 //# sourceMappingURL=index.js.map
